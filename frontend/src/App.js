5ac448b38fe7edf09e2d4a52a27dcc8ce2f26c17
Hemant Saini <hsainioff@gmail.com>
2020-12-09 22:23:29 +0530

    App.js Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
 
 const App = () => {
   return (
-    <>Hello</>
+    <>
+      <Header/>
+      <main>
+        <HomeScreen/>
+      </main>
+      <Footer/>
+    </>
   );
 }
 
