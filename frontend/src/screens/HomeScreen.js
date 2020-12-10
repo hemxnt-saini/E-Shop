c4b94f26b6196ebf3e802d64aa1a6a4fe022edc0
Hemant Saini <hsainioff@gmail.com>
2020-12-10 21:32:57 +0530

    HomeScreen Component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import {Col,Row} from 'react-bootstrap'
+import Product from '../components/Product'
+import products from '../products'
 const HomeScreen = () => {
     return (
-        <div>
-            HomeScreen
-        </div>
+        <>
+        <h1>Latest Products</h1>
+            <Row>
+                {products.map((product) => (
+                    <Col sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product}/>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
