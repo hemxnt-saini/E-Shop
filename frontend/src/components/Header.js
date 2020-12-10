e9bd9400b3807036bb24a4255cca3554ce8cc63e
Hemant Saini <hsainioff@gmail.com>
2020-12-10 21:32:01 +0530

    Header Component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import {Navbar,Nav, Container} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 const Header = () => {
     return (
         <header>
-            <nav>
-                Header
-            </nav>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>E-SHOP</Navbar.Brand>
+                    </LinkContainer>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                    <LinkContainer to="/cart">
+                        <Nav.Link><i className="fa fa-shopping-cart"/> Cart</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/login">
+                        <Nav.Link><i className="fa fa-user" />Login</Nav.Link>
+                    </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     )
 }
