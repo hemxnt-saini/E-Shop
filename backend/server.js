8b67e4244d741fc4d4b744731a50a3041f1cd876
Hemant Saini <hsainioff@gmail.com>
2020-12-13 18:10:57 +0530

    Backend Initialise

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import products from './data/products.js'
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('API Running..')
+})
+
+app.get('/api/products', (req,res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req,res) => {
+    const product = products.find(p=> p._id === req.params.id)
+    res.json(product)
+})
+
+
+app.listen(5000,console.log("Server Running..."));
