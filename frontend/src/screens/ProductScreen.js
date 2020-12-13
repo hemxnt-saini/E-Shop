9d3c72399f42799a63eaa877a27cdd1bf56cc888
Hemant Saini <hsainioff@gmail.com>
2020-12-13 18:12:30 +0530

    ProductScreen rendered from Backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import products from '../products'
+import React, {useState,useEffect} from 'react'
 import {Row,Col,Image,ListGroup,Card,Button} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import Rating from '../components/Rating'
+import axios from 'axios'
 
 const ProductScreen = ({match}) => {
-    const product = products.find((p) => p._id === match.params.id) 
+
+    const [product, setProduct] = useState({})
+
+    useEffect(() =>{
+        const fetchProduct = async () => {
+            const {data} = await axios.get(`/api/products/${match.params.id}`) 
+            setProduct(data);
+        }
+        fetchProduct()
+    },[])
+
 return ( 
     <>
     <Link to ="/" className="btn btn-light my-3">Go Back</Link>
