f0614997b8bc57d4e003c04bf1d6918b4e32bf93
Hemant Saini <hsainioff@gmail.com>
2020-12-14 13:56:05 +0530

    Changed wrt dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import products from './data/products.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express();
 
 app.get('/', (req,res) => {
@@ -16,5 +18,6 @@ app.get('/api/products/:id', (req,res) => {
     res.json(product)
 })
 
+const PORT = process.env.PORT || 5000
 
-app.listen(5000,console.log("Server Running..."));
+app.listen(PORT,console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`));
