4ba481f9380f92d69257991daf6dac1cdc782834
Hemant Saini <hsainioff@gmail.com>
2020-12-15 13:50:51 +0530

    ConnectDB with MongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+import color from 'colors'
+
+const connectDB = async () => {
+
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI,{
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
+    } catch (error) {
+        console.error(`Error: ${error.message}`.red.underline.bold);
+        process.exit(1);
+    }
+}
+
+export default connectDB
