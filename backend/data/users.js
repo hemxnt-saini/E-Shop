556108160a2dd09abfcf2d29b3aeafb78fbeb4ad
Hemant Saini <hsainioff@gmail.com>
2020-12-19 14:16:40 +0530

    Password Hashed

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,6 +4,7 @@ const users = [
         name: 'Admin User',
         email: 'admin@example.com',
         password: bcrypt.hashSync('123456',10),
+        isAdmin: true
     },
     {
         name: 'John Doe',
