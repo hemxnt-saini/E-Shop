ea8003a7f219cdc587617899b43a63533d8f866b
Hemant Saini <hsainioff@gmail.com>
2020-12-20 09:37:15 +0530

    Basic server Comments

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,26 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 
-dotenv.config()
-connectDB()
 const app = express();
 
+//ENV Access
+dotenv.config()
+
+//DB Connect
+connectDB()
+
+//Home Route
 app.get('/', (req,res) => {
     res.send('API Running..')
 })
 
+//Routes
 app.use('/api/products', productRoutes)
 
+//Middlewares
 app.use(notFound)
 app.use(errorHandler)
 
+//PORT & Listen
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT,console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.underline.bold));
