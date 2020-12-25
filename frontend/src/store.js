9ddbe993d5819e9f10e6006248c758eacbebb5a8
Hemant Saini <hsainioff@gmail.com>
2020-12-25 11:43:10 +0530

    CartReducer in Combine Reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,15 +2,21 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {productListReducer,productDetailsReducer} from './reducers/productReducers'
+import {cartReducer} from './reducers/cartReducers'
 
 //CombineReducers
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer, 
+    cart: cartReducer, 
 })
 
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
 //Initial State
-const initialState ={}
+const initialState ={
+    cart: {cartItems: cartItemsFromStorage}
+}
 
 //Middlewares
 const middleware = [thunk]
