5ddf7a1de5371b3148a2f8198689e8c5e56aa976
Hemant Saini <hsainioff@gmail.com>
2020-12-25 11:40:52 +0530

    Calling Cart Actions

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,37 @@
+import React, {useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import {useDispatch,useSelector} from 'react-redux'
+import {Row, Col,ListGroup,Image,Form,Button,Card} from 'react-bootstrap'
+import Message from '../components/Message'
+import {addToCart,removeFromCart} from '../actions/cartActions'
+
+
+const CartScreen = ({match,location,history}) => {
+    const productId = match.params.id
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const {cartItems} = cart
+
+    useEffect(()=>{
+        if(productId){
+            dispatch(addToCart(productId,qty))
+        } 
+    },[dispatch,productId,qty])
+
+const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id))
+}
+
+const checkoutHandler = () => {
+    history.push('/login?redirect=shipping')
+}
+
+    return (
+        <div>Cart</div>
+    )
+}
+
+export default CartScreen
