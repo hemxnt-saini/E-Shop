ecdeaa4c26eb1fa25f3c14527f3d698842396144
Hemant Saini <hsainioff@gmail.com>
2020-12-25 11:42:47 +0530

    Css Handling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import store from './store'
-import './index.css';
 import './bootstrap.min.css';
+import './index.css';
 import App from './App';
 
 ReactDOM.render(
