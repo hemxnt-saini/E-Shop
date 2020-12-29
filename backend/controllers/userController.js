8cd04def2fb265b012f1508f731ed4407b38e655
Hemant Saini <hsainioff@gmail.com>
2020-12-29 11:09:01 +0530

    registerUser & getUserProfile controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,4 +24,60 @@ const authUser = asyncHandler(async (req,res) => {
     }
 })
 
-export {authUser}
+
+//Register a User
+//POST /api/users
+//PUBLIC
+const registerUser = asyncHandler(async (req,res) => {
+    const {name,email,password} = req.body
+    
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User Already Exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin:user.isAdmin,
+            token:genrateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
+})
+
+
+//Get Logged in user 
+//GET /api/users/profile
+//PRIVATE- Protected Route(Need Token)
+const getUserProfile = asyncHandler(async (req,res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user){
+        res.json({
+            _id: user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin:user.isAdmin,
+        })
+    }else{
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+
+export {authUser, getUserProfile, registerUser}   
