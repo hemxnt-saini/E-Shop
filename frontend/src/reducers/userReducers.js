147ac902c5432e0f6bd14de9ec2fc2a675e50559
Hemant Saini <hsainioff@gmail.com>
2021-01-03 19:14:46 +0530

    User Register Reducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,4 +1,4 @@
-import {USER_LOGIN_FAIL,USER_LOGIN_SUCCESS,USER_LOGIN_REQUEST,USER_LOGOUT} from '../constants/userConstants'
+import {USER_LOGIN_FAIL,USER_LOGIN_SUCCESS,USER_LOGIN_REQUEST,USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL} from '../constants/userConstants'
 
 
 export const userLoginReducer = (state = {},action) =>{
@@ -15,6 +15,24 @@ export const userLoginReducer = (state = {},action) =>{
         
         case USER_LOGOUT:
             return {}
+
+        default: 
+            return state
+    }
+}
+
+//REGISTER REDUCER
+export const userRegisterReducer = (state = {},action) =>{
+    switch(action.type){
+
+        case USER_REGISTER_REQUEST:
+            return {loading:true}
+            
+        case USER_REGISTER_SUCCESS:
+            return {loading:false, userInfo:action.payload}
+        
+        case USER_REGISTER_FAIL:
+            return {loading:false, error:action.payload}
 
         default: 
             return state
