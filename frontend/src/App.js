93fb94eed1e942d071a8eed1d6178e678cef15d4
Hemant Saini <hsainioff@gmail.com>
2021-01-03 19:18:00 +0530

    RegisterComponent Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 import { Container } from 'react-bootstrap'
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 
@@ -16,6 +17,7 @@ const App = () => {
       <main className="my-4">
       <Container>
         <Route path ="/login" component={LoginScreen}/>
+        <Route path ="/register" component={RegisterScreen}/>
         <Route path ="/product/:id" component={ProductScreen}/>
         <Route path ="/cart/:id?" component={CartScreen}/>
         <Route path ="/" component={HomeScreen} exact/>
