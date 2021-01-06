98350c40923fcd2822fa35ac9be2d84bd4525b67
Hemant Saini <hsainioff@gmail.com>
2021-01-06 12:23:46 +0530

    OrderReducer in combinedReduer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import {productListReducer,productDetailsReducer} from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
 import {userLoginReducer,userRegisterReducer,userDetailsReducer,userUpdateProfileReducer} from './reducers/userReducers'
+import {orderCreateReducer} from './reducers/orderReducers'
 
 //CombineReducers
 const reducer = combineReducers({
@@ -14,6 +15,7 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
