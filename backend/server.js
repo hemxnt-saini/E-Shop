9698f66fe744e798d24f88e0b5d598564c458e60
Hemant Saini <hsainioff@gmail.com>
2021-01-06 12:21:45 +0530

    orderController Route in server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 
 
@@ -24,6 +25,7 @@ app.get('/', (req,res) => {
 //Routes
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 //Middlewares
 app.use(notFound)
