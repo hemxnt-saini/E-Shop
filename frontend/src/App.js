e43cc5f35987abafbcd788170140d325735cc863
Hemant Saini <hsainioff@gmail.com>
2021-01-06 12:25:22 +0530

    Route for PlaceOrder Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import { Container } from 'react-bootstrap'
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 
@@ -22,6 +23,7 @@ const App = () => {
         <Route path ="/login" component={LoginScreen}/>
         <Route path ="/shipping" component={ShippingScreen}/>
         <Route path ="/payment" component={PaymentScreen}/>
+        <Route path ="/placeorder" component={PlaceOrderScreen}/>
         <Route path ="/register" component={RegisterScreen}/>
         <Route path ="/profile" component={ProfileScreen}/>
         <Route path ="/product/:id" component={ProductScreen}/>
