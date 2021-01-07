fa14a5314c53cfec89b02dae02eac63ebdf57007
Hemant Saini <hsainioff@gmail.com>
2021-01-07 08:34:50 +0530

    Model Error Change

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -28,7 +28,7 @@ const orderSchema = mongoose.Schema({
     },
     taxPrice:{type:Number,default:0.0},
     shippingPrice:{type:Number,default:0.0},
-    TotalPrice:{type:Number,default:0.0},
+    totalPrice:{type:Number,default:0.0},
     isPaid:{type:Boolean,default:false,required: true},
     paidAt:{type:Date},
     isDelivered:{type:Boolean,required: true,default:false},
