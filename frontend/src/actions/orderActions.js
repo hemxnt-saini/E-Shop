c59ac4f2a21740c8bf52f29a4b7c070cef7abbc0
Hemant Saini <hsainioff@gmail.com>
2021-01-08 10:42:33 +0530

    Order_Pay Action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -57,3 +57,31 @@ export const getOrderDetails = (id) => async(dispatch, getState) =>{
     }
 }
 
+
+export const payOrder = (orderId, paymentResult) => async(dispatch, getState) =>{
+    try {
+        dispatch({type :ORDER_PAY_REQUEST})
+
+        const {userLogin : {userInfo}} = getState()
+
+        const config= { 
+            headers: {
+                'Content-Type':'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.put(`/api/orders/${orderId}/pay`, paymentResult, config)
+
+        dispatch({
+            type: ORDER_PAY_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type : ORDER_PAY_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
