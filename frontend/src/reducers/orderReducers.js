5fb714e333d19fb9438505d703545316ab281c1b
Hemant Saini <hsainioff@gmail.com>
2021-01-08 10:41:58 +0530

    Order_Pay Reducers

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -31,3 +31,21 @@ export const orderDetailsReducer = (state={loading:true, orderItems:[], shipping
             return state
     }
 }
+
+export const orderPayReducer = (state={}, action) =>{
+    switch(action.type){
+        case ORDER_PAY_REQUEST:
+            return{loading: true}
+        
+        case ORDER_PAY_SUCCESS:
+            return{loading: false, success: true}
+
+        case ORDER_PAY_FAIL:
+            return{loading: false, error: action.payload}
+
+        case ORDER_PAY_RESET:
+            return {}
+        default:
+            return state
+    }
+}
