27f4faa3672186d8ee71131f3d3469f24b8cdac0
Hemant Saini <hsainioff@gmail.com>
2021-01-09 12:14:44 +0530

    Routes for order & userController

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
 import {protect} from '../middleware/authMiddleware.js'
-import {addOrderItems,getMyOrders,getOrderById,updateOrderToPaid} from '../controllers/orderContoller.js'
+import {addOrderItems,getMyOrders,getOrderById,updateOrderToPaid} from '../controllers/orderController.js'
 
 router.route('/').post(protect,addOrderItems)
 router.route('/myorders').get(protect,getMyOrders)
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import {authUser,getUserProfile, registerUser, updateUserProfile} from '../controllers/userController.js'
-import {protect} from '../middleware/authMiddleware.js'
+import {authUser,getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser} from '../controllers/userController.js'
+import {protect,admin} from '../middleware/authMiddleware.js'
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect,admin,getUsers)
+router.route('/:id').delete(protect,admin,deleteUser)
 router.route('/login').post(authUser)
-router.route('/profile').get(protect,getUserProfile)
-router.route('/profile').put(protect,updateUserProfile)
+router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile)
 
 export default router
