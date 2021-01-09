7932f65c474a99985ae224217402d96aef1d9d34
Hemant Saini <hsainioff@gmail.com>
2021-01-09 12:23:03 +0530

    Header for AdminOnly

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -38,10 +38,31 @@ const Header = () => {
                                     Logout
                                 </NavDropdown.Item>
                             </NavDropdown>  
-                        ) : <LinkContainer to="/login">
+                        ) : ( 
+                        <LinkContainer to="/login">
                         <Nav.Link><i className="fa fa-user" /> Sign In</Nav.Link>
                         </LinkContainer>
+                        )
                     }
+                    {userInfo && userInfo.isAdmin && (
+                        <NavDropdown title='admin' id="adminmenu">
+                        <LinkContainer to="/admin/userlist">
+                            <NavDropdown.Item>
+                                Users
+                            </NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to="/admin/productlist">
+                            <NavDropdown.Item>
+                                Products
+                            </NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to="/admin/orderlist">
+                            <NavDropdown.Item>
+                                Orders
+                            </NavDropdown.Item>
+                        </LinkContainer>
+                    </NavDropdown>
+                    )}
                     </Nav>
                 </Navbar.Collapse>
                 </Container>
