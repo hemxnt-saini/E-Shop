cfbc9f52aa51d4860dbe0bf303586abdcba09811
Hemant Saini <hsainioff@gmail.com>
2021-01-09 12:30:15 +0530

    UserListScreen Route App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
+import UserListScreen from './screens/UserListScreen'
 import { Container } from 'react-bootstrap'
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 
@@ -30,6 +31,7 @@ const App = () => {
         <Route path ="/profile" component={ProfileScreen}/>
         <Route path ="/product/:id" component={ProductScreen}/>
         <Route path ="/cart/:id?" component={CartScreen}/>
+        <Route path ="/admin/userlist" component={UserListScreen}/>
         <Route path ="/" component={HomeScreen} exact/>
       </Container>
       </main>
