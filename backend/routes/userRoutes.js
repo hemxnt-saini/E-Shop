58771a5d239ca7db02656f61ce058d9601e27042
Hemant Saini <hsainioff@gmail.com>
2021-01-10 08:05:14 +0530

    UpdateUser Route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,22 @@
 import express from 'express'
 const router = express.Router()
-import {authUser,getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser} from '../controllers/userController.js'
+import {authUser,getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser} from '../controllers/userController.js'
 import {protect,admin} from '../middleware/authMiddleware.js'
 
-router.route('/').post(registerUser).get(protect,admin,getUsers)
-router.route('/:id').delete(protect,admin,deleteUser)
-router.route('/login').post(authUser)
-router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile)
+router.route('/')
+        .post(registerUser)
+        .get(protect,admin,getUsers)
+
+router.route('/:id')
+        .delete(protect,admin,deleteUser)
+        .get(protect,admin,getUserById)
+        .put(protect,admin,updateUser)
+
+router.route('/login')
+        .post(authUser)
+
+router.route('/profile')
+        .get(protect,getUserProfile)
+        .put(protect,updateUserProfile)
 
 export default router
