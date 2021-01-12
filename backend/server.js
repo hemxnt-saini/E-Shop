db0355d9261f89c81c2170f4d61356ae3114f8ba
Hemant Saini <hsainioff@gmail.com>
2021-01-12 10:24:44 +0530

    Uploading Route

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 
 
@@ -26,8 +28,12 @@ app.get('/', (req,res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req,res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname,'/uploads')))
 
 //Middlewares
 app.use(notFound)
