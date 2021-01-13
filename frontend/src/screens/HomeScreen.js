99a475386a981b17919a82d137eba3d6fedecb76
Hemant Saini <hsainioff@gmail.com>
2021-01-13 10:55:57 +0530

    SearchBox Added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,15 +6,18 @@ import {listProducts} from './../actions/productActions'
 import Message from './../components/Message'
 import Loader from './../components/Loader'
 
-const HomeScreen = () => {
+const HomeScreen = ({match}) => {
+
+    const keyword = match.params.keyword
+
     const dispatch = useDispatch()
     
     const productList = useSelector(state => state.productList)
     const {loading,error,products} = productList
 
     useEffect(() => {
-        dispatch(listProducts())
-        }, [dispatch]);
+        dispatch(listProducts(keyword))
+        }, [dispatch, keyword]);
 
     return (
         <>
