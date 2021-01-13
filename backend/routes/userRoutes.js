70ccfb63c04d36a1c2ba545c73a2aa8232ce1bcd
Hemant Saini <hsainioff@gmail.com>
2021-01-13 09:35:15 +0530

    Routes checked

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,22 +1,27 @@
 import express from 'express'
 const router = express.Router()
-import {authUser,getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser} from '../controllers/userController.js'
-import {protect,admin} from '../middleware/authMiddleware.js'
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/')
-        .post(registerUser)
-        .get(protect,admin,getUsers)
-
-router.route('/:id')
-        .delete(protect,admin,deleteUser)
-        .get(protect,admin,getUserById)
-        .put(protect,admin,updateUser)
-
-router.route('/login')
-        .post(authUser)
-
-router.route('/profile')
-        .get(protect,getUserProfile)
-        .put(protect,updateUserProfile)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
+router.post('/login', authUser)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
 export default router
