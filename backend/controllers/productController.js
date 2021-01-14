23c4b57d9a6cceb6f34420135b2be3ee14ee2a3d
Hemant Saini <hsainioff@gmail.com>
2021-01-14 19:20:06 +0530

    Pagination Functionality in controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,6 +5,9 @@ import asyncHandler from 'express-async-handler'
 //Fetch All Products
 //GET /api/products PUBLIC
 const getProducts = asyncHandler(async (req,res) => {
+    const pageSize = 10
+    const page = Number(req.query.pageNumber) || 1
+
     const keyword = req.query.keyword ? {
         name: {
             $regex: req.query.keyword,  //Anyword related search
@@ -12,8 +15,9 @@ const getProducts = asyncHandler(async (req,res) => {
         }
     } : {}
 
-    const products = await Product.find({...keyword})
-    res.json(products)
+    const count = await Product.countDocuments({...keyword})
+    const products = await Product.find({...keyword}).limit(pageSize).skip(pageSize * (page - 1))
+    res.json({products, page, pages: Math.ceil(count / pageSize)})
 })
 
 //Fetch Product by ID
