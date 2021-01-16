1a06c4dc39a429a1dd742e5dfad6a8e43df555a6
Hemant Saini <hsainioff@gmail.com>
2021-01-16 09:54:14 +0530

    Making build folder static

--- backend/server.js
+++ backend/server.js
@@ -16,24 +16,32 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
 //Body Parse MW
 app.use(express.json())
-
-//Home Route
-app.get('/', (req,res) => {
-    res.send('API Running..')
-})
 
 //Routes
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
-
 app.get('/api/config/paypal', (req,res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
+//Making Uploads Static folder
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname,'/uploads')))
+
+//Build
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname,'frontend', 'build', 'index.html'))
+    )
+}else{
+    app.get('/', (req,res) => {
+        res.send('API Running..')
+    })
+}
 
 //Middlewares
 app.use(notFound)
